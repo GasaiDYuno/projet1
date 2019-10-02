a3c5e5941cae5a9c2f7cec1e236c7d619c3b345c
GasaiDYuno <54894971+GasaiDYuno@users.noreply.github.com>
2019-10-03 00:02:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+                                    /* Darkmode */
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+                                    /* fin du Darkmode */
+
+
---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "globDirectory": "C:\\Users\\annub\\progra\\Projet 1\\assets\\js",
+  "globPatterns": [
+    "**/*.{jpg,html,js,css,json}"
+  ],
+  "swDest": "C:\\Users\\annub\\progra\\Projet 1\\assets\\js\\sw.js"
+  "swSrc": "src-sw.js"
+};
+
